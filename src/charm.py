cbba45bcb9ae81e03bbec3e910e02599a67dd09c
WRFitch <will.fitch@canonical.com>
2022-07-29 18:24:52 +0100

    more fixes for trigger_db_relations

--- src/charm.py
+++ src/charm.py
@@ -449,7 +449,10 @@ class PgBouncerK8sCharm(CharmBase):
 
     def trigger_db_relations(self):
         """Triggers every instance of client relations db and db-admin, if they exist."""
-        # TODO self.model.relations.get() is having trouble accessing relations
+        if not self.backend_postgres:
+            return
+
+        # TODO self.model.relations.get() is having trouble accessing relations when they don't exist.
         if not self.model.relations or len(self.model.relations) == 0:
             return
 
