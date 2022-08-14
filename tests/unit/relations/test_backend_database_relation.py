e15be9a793dfd63a39c7905884e4cf6fbbe4d70e
WRFitch <will.fitch@canonical.com>
2022-08-14 22:14:55 +0100

    experimenting with unit tests

--- tests/unit/relations/test_backend_database_relation.py
+++ tests/unit/relations/test_backend_database_relation.py
@@ -8,10 +8,7 @@ from ops.testing import Harness
 
 from charm import PgBouncerK8sCharm
 
-TEST_UNIT = {
-    "master": "host=master port=1 dbname=testdatabase",
-    "standbys": "host=standby1 port=1 dbname=testdatabase",
-}
+from charms.pgbouncer_k8s.v0.pgb import PgbConfig, DEFAULT_CONFIG
 
 BACKEND_RELATION_NAME = "backend-database"
 
@@ -25,21 +22,25 @@ class TestBackendDatabaseRelation(unittest.TestCase):
         self.charm = self.harness.charm
         self.app = self.charm.app.name
         self.unit = self.charm.unit.name
-        self.relation = self.charm.backend
+        self.backend = self.charm.backend
 
         # Define a backend relation
         self.rel_id = self.harness.add_relation(BACKEND_RELATION_NAME, "postgres")
         self.harness.add_relation_unit(self.rel_id, "postgres/0")
         self.harness.add_relation_unit(self.rel_id, self.unit)
 
-    @patch("charm.PgBouncerK8sCharm.read_pgb_config")
-    def test_on_database_created(self, _cfg):
+    @patch("relations.backend_database.BackendDatabaseRequires.get_postgres")
+    @patch("charms.pgbouncer_k8s.v0.pgb.generate_password", return_value="pw")
+    @patch("relations.backend_database.BackendDatabaseRequires.initialise_auth_function")
+    @patch("charm.PgBouncerK8sCharm.read_pgb_config", return_value = PgbConfig(DEFAULT_CONFIG))
+    def test_on_database_created(self, _cfg, _init_auth, _gen_pw, _postgres):
         # TODO update to use relations the way Marcelo did
+        pw = _gen_pw.return_value
         mock_event = MagicMock()
-        mock_event.username = "new-user"
-        mock_event.password = "new-user-password"
 
         self.charm.backend._on_database_created(mock_event)
+        _postgres.create_user.assert_called_with(self.backend.auth_user, pw, admin=True)
+        _init_auth.assert_called_with(_postgres, self.backend.database.database)
 
     def test_relation_departed(self):
         self.charm.backend._on_relation_departed(MagicMock())
@@ -54,12 +55,12 @@ class TestBackendDatabaseRelation(unittest.TestCase):
         postgres = MagicMock()
         dbname = "test-db"
 
-        self.charm.backend.initialise_auth_function(postgres=postgres, dbname=dbname)
+        self.backend.initialise_auth_function(postgres=postgres, dbname=dbname)
 
         postgres.connect_to_database.assert_called_with(dbname)
         conn = postgres.connect_to_database().__enter__()
         cursor = conn.cursor().__enter__()
         cursor.execute.assert_called_with(
-            install_script.replace("auth_user", self.charm.backend.auth_user)
+            install_script.replace("auth_user", self.backend.auth_user)
         )
         conn.close.assert_called()
