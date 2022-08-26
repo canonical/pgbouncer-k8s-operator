beebbd4c9f8830b9a0aa4c2108314a08729ba869
WRFitch <will.fitch@canonical.com>
2022-08-26 18:50:49 +0100

    happy linter

--- src/charm.py
+++ src/charm.py
@@ -21,7 +21,7 @@ from ops.charm import CharmBase, ConfigChangedEvent, PebbleReadyEvent, StartEven
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
-from ops.pebble import ConnectionError, Layer, PathError
+from ops.pebble import Layer, PathError
 
 from constants import AUTH_FILE_PATH, INI_PATH, PG_USER, PGB
 from relations.backend_database import BackendDatabaseRequires
@@ -177,7 +177,7 @@ class PgBouncerK8sCharm(CharmBase):
         services = pgb_container.get_services()
         if PGB not in services.keys():
             # pebble_ready event hasn't fired so pgbouncer has not been added to pebble config
-            # TODO raise an error here so we can handle when this is called before pebble config is ready.
+            # TODO raise an error so we can handle reloads before pebble config is ready.
             return
 
         self.unit.status = MaintenanceStatus("Reloading Pgbouncer")
