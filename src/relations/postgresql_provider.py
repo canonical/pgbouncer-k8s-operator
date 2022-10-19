a261313105c4ea00976f87edddce936263c29897
WRFitch <will.fitch@canonical.com>
2022-10-19 23:53:50 +0100

    added config management to new relation

--- src/relations/postgresql_provider.py
+++ src/relations/postgresql_provider.py
@@ -19,7 +19,7 @@ from charms.postgresql_k8s.v0.postgresql import (
 )
 from ops.charm import CharmBase, RelationBrokenEvent
 from ops.framework import Object
-from ops.model import BlockedStatus, WaitingStatus
+from ops.model import Application, BlockedStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
@@ -61,7 +61,7 @@ class PostgreSQLProvider(Object):
         )
 
     def _on_database_requested(self, event: DatabaseRequestedEvent) -> None:
-        """Handle the legacy postgresql-client relation changed event.
+        """Handle the client relation-requested event.
 
         Generate password and handle user and database creation for the related application.
         """
@@ -115,10 +115,47 @@ class PostgreSQLProvider(Object):
             self.charm.unit.status = BlockedStatus(
                 f"Failed to initialize {self.relation_name} relation"
             )
+            return
+
+        # set up auth function
+        self.charm.backend.initialise_auth_function(dbname=database)
+
+        # Create user in pgbouncer config
+        cfg = self.charm.read_pgb_config()
+        cfg.add_user(user, admin=True if "SUPERUSER" in extra_user_roles else False)
+
+        # Update endpoints
+
+        # In postgres, "endpoints" will only ever have one value. Other databases using the library
+        # can have more, but that's not planned for the postgres charm.
+        postgres_endpoint = self.charm.backend.postgres_databag.get("endpoints")
+        cfg = self.charm.read_pgb_config()
+        cfg["databases"][database] = {
+            "host": postgres_endpoint.split(":")[0],
+            "dbname": database,
+            "port": postgres_endpoint.split(":")[1],
+            "auth_user": self.charm.backend.auth_user,
+        }
+
+        # TODO should postgres scaling be handled by this relation? Ideally, in future (after pgb
+        # version 1.17, therefore after the snap) this entry will point to multiple endpoints,
+        # which will be automatically load balanced by default. Incidentally, this _standby suffix
+        # should therefore be removed.
+        read_only_endpoint = self._get_read_only_endpoint()
+        if read_only_endpoint:
+            cfg["databases"][f"{database}_standby"] = {
+                "host": read_only_endpoint.split(":")[0],
+                "dbname": database,
+                "port": read_only_endpoint.split(":")[1],
+                "auth_user": self.charm.backend.auth_user,
+            }
+        else:
+            cfg["databases"].pop(f"{database}_standby", None)
+        # Write config data to charm filesystem
+        self.charm.render_pgb_config(cfg, reload_pgbouncer=True)
 
     def _on_relation_broken(self, event: RelationBrokenEvent) -> None:
-        """Remove the user created for this relation."""
-        # Check for some conditions before trying to access the PostgreSQL instance.
+        """Remove the user created for this relation, and revoke connection permissions."""
         if not self.charm.backend.postgres:
             # We can't relate an app to the backend database without a backend postgres relation
             wait_str = "waiting for backend-database relation to connect"
@@ -130,10 +167,17 @@ class PostgreSQLProvider(Object):
         if not self.charm.unit.is_leader():
             return
 
-        # Delete the user.
+        cfg = self.charm.read_pgb_config()
+        database = event.relation.data[self.get_external_app(event.relation)].get("database")
+        cfg["databases"].pop(database, None)
+        cfg["databases"].pop(f"{database}_standby", None)
         user = f"relation_id_{event.relation.id}"
+        cfg.remove_user(user)
+        self.charm.render_pgb_config(cfg, reload_pgbouncer=True)
+
+        # Delete the user.
         try:
-            self.charm.postgresql.delete_user(user)
+            self.charm.backend.postgres.delete_user(user)
         except PostgreSQLDeleteUserError as e:
             logger.exception(e)
             self.charm.unit.status = BlockedStatus(
@@ -161,3 +205,25 @@ class PostgreSQLProvider(Object):
                 relation.id,
                 endpoints,
             )
+
+    def _get_read_only_endpoint(self):
+        """Get a read-only-endpoint from backend relation.
+
+        Though multiple readonly endpoints can be provided by the new backend relation, only one
+        can be consumed by this legacy relation.
+
+        TODO this is copy-pasted from the db relation, clean it up a bit.
+        """
+        read_only_endpoints = self.charm.backend.postgres_databag.get("read-only-endpoints")
+        if read_only_endpoints is None or len(read_only_endpoints) == 0:
+            return None
+        return read_only_endpoints.split(",")[0]
+
+    def get_external_app(self, relation):
+        """Gets external application, as an Application object.
+
+        TODO this is stolen from the db relation - cleanup
+        """
+        for entry in relation.data.keys():
+            if isinstance(entry, Application) and entry != self.charm.app:
+                return entry
