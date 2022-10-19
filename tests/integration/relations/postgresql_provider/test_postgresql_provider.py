f34bbc8713c86ce5c35053d4175a145810f44cef
WRFitch <will.fitch@canonical.com>
2022-10-19 01:01:33 +0100

    fix helper import

--- tests/integration/relations/postgresql_provider/test_postgresql_provider.py
+++ tests/integration/relations/postgresql_provider/test_postgresql_provider.py
@@ -11,14 +11,14 @@ import yaml
 from pytest_operator.plugin import OpsTest
 
 from constants import PGB
-from tests.integration.new_relations.helpers import (
-    build_connection_string,
-    check_relation_data_existence,
-    get_application_relation_data,
-)
 from tests.integration.postgresql_helpers import (
     check_database_users_existence,
     scale_application,
+)
+from tests.integration.postgresql_provider.helpers import (
+    build_connection_string,
+    check_relation_data_existence,
+    get_application_relation_data,
 )
 
 logger = logging.getLogger(__name__)
