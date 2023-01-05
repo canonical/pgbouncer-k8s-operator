a9faa8893ad10e8bc6cca26d3923278070ba8497
WRFitch <will.fitch@canonical.com>
2023-01-05 16:17:10 +0000

    added a wait to smooth out testing

--- tests/integration/relations/pgbouncer_provider/test_pgbouncer_provider.py
+++ tests/integration/relations/pgbouncer_provider/test_pgbouncer_provider.py
@@ -199,6 +199,7 @@ async def test_database_admin_permissions(ops_test: OpsTest):
 async def test_no_read_only_endpoint_in_standalone_cluster(ops_test: OpsTest):
     """Test that there is no read-only endpoint in a standalone cluster."""
     await scale_application(ops_test, PGB, 1)
+    await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", timeout=600)
     await check_new_relation(
         ops_test,
         unit_name=ops_test.model.applications[CLIENT_APP_NAME].units[0].name,
@@ -217,6 +218,7 @@ async def test_no_read_only_endpoint_in_standalone_cluster(ops_test: OpsTest):
 async def test_read_only_endpoint_in_scaled_up_cluster(ops_test: OpsTest):
     """Test that there is read-only endpoint in a scaled up cluster."""
     await scale_application(ops_test, PGB, 2)
+    await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", timeout=600)
     await check_new_relation(
         ops_test,
         unit_name=ops_test.model.applications[CLIENT_APP_NAME].units[0].name,
