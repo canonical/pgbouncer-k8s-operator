44b9a09589cb0ca7316cdb2146043e1fc0a6f98c
Dragomir Penev <dragomir.penev@canonical.com>
2023-04-12 23:25:45 +0300

    TLS test tweak

--- tests/integration/relations/test_backend_database.py
+++ tests/integration/relations/test_backend_database.py
@@ -140,11 +140,12 @@ async def test_tls_encrypted_connection_to_postgres(ops_test: OpsTest):
         # Check the logs to ensure TLS is being used by PgBouncer.
         postgresql_primary_unit = await get_postgres_primary(ops_test)
         logs = await run_command_on_unit(
-            ops_test, postgresql_primary_unit, "/charm/bin/pebble logs -n=all"
+            ops_test,
+            postgresql_primary_unit,
+            'grep "database=waltz SSL enabled" /var/log/postgresql/postgresql.log',
         )
         assert (
-            f"connection authorized: user={pgb_user} database=waltz"
-            " SSL enabled (protocol=TLSv1.3, cipher=TLS_AES_256_GCM_SHA384, bits=256)" in logs
+            f"connection authorized: user={pgb_user} database=waltz SSL enabled" in logs
         ), "TLS is not being used on connections to PostgreSQL"
 
 
