1d250246e82570bcda206e2ba052cf54e4582a07
Dragomir Penev <dragomir.penev@canonical.com>
2023-06-01 14:01:14 +0300

    Separate dir for pgb

--- src/constants.py
+++ src/constants.py
@@ -7,7 +7,7 @@
 PGB = "pgbouncer"
 PG = PG_USER = PG_GROUP = "postgres"
 
-PGB_DIR = "/var/lib/postgresql/pgbouncer"
+PGB_DIR = "/var/lib/pgbouncer"
 INI_PATH = f"{PGB_DIR}/pgbouncer.ini"
 AUTH_FILE_PATH = f"{PGB_DIR}/userlist.txt"
 
