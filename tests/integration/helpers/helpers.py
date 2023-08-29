3611946648fa814a5796e3837437595da43d507d
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2023-08-30 00:38:22 +0300

    Fixes

--- tests/integration/helpers/helpers.py
+++ tests/integration/helpers/helpers.py
@@ -295,7 +295,7 @@ async def check_tls(ops_test: OpsTest, relation_id: int, enabled: bool) -> bool:
     cleint_name = await app_name(ops_test, CLIENT_APP_NAME)
     unit = ops_test.model.applications[cleint_name].units[0]
     params = {
-        "dbname": "application_first_database",
+        "dbname": f"{CLIENT_APP_NAME.replace("-", "_")}_first_database",
         "relation-id": relation_id,
         "relation-name": "first-database",
         "readonly": False,
