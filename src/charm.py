d5233a56b2426590695ced586a912731391060f6
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2024-02-08 19:28:51 +0200

    [MISC] Fix secret keys (#212)
    * Fix secret keys
    
    * Update secrets loop
    
    * No deferring on missing tls
    
    * Wrong footgun check key

--- src/charm.py
+++ src/charm.py
@@ -72,9 +72,9 @@ class PgBouncerK8sCharm(CharmBase):
             self,
             relation_name=PEER_RELATION_NAME,
             additional_secret_fields=[
-                AUTH_FILE_DATABAG_KEY,
-                CFG_FILE_DATABAG_KEY,
-                MONITORING_PASSWORD_KEY,
+                self._translate_field_to_secret_key(AUTH_FILE_DATABAG_KEY),
+                self._translate_field_to_secret_key(CFG_FILE_DATABAG_KEY),
+                self._translate_field_to_secret_key(MONITORING_PASSWORD_KEY),
             ],
             secret_field_name=SECRET_INTERNAL_LABEL,
             deleted_label=SECRET_DELETED_LABEL,
@@ -270,16 +270,7 @@ class PgBouncerK8sCharm(CharmBase):
             event.defer()
             return
 
-        tls_enabled = all(self.tls.get_tls_files())
-        if self.model.relations.get("certificates", []) and not tls_enabled:
-            logger.debug(
-                "pgbouncer_pebble_ready: Deferring as certificates files are not yet populated for existing certificates relation"
-            )
-            self.unit.status = WaitingStatus("Waiting for certificates")
-            event.defer()
-            return
-        # in case of pod restart
-        elif tls_enabled:
+        if all(self.tls.get_tls_files()):
             self.push_tls_files_to_workload(False)
 
         pebble_layer = self._pgbouncer_layer()
@@ -290,13 +281,14 @@ class PgBouncerK8sCharm(CharmBase):
 
         self.unit.set_workload_version(self.version)
 
-        # Update postgres endpoints in config to match the current state of the charm.
-        self.update_postgres_endpoints(reload_pgbouncer=True)
+        if self.unit.is_leader():
+            # Update postgres endpoints in config to match the current state of the charm.
+            self.update_postgres_endpoints(reload_pgbouncer=True)
 
-        if JujuVersion.from_environ().supports_open_port_on_k8s:
-            self.unit.open_port("tcp", self.config["listen_port"])
-        else:
-            self._patch_port()
+            if JujuVersion.from_environ().supports_open_port_on_k8s:
+                self.unit.open_port("tcp", self.config["listen_port"])
+            else:
+                self._patch_port()
 
     def _on_config_changed(self, event: ConfigChangedEvent) -> None:
         """Handle changes in configuration.
@@ -544,6 +536,13 @@ class PgBouncerK8sCharm(CharmBase):
         if scope == UNIT_SCOPE:
             return self.unit
 
+    def peer_relation_data(self, scope: Scopes) -> DataPeer:
+        """Returns the peer relation data per scope."""
+        if scope == APP_SCOPE:
+            return self.peer_relation_app
+        elif scope == UNIT_SCOPE:
+            return self.peer_relation_unit
+
     def _translate_field_to_secret_key(self, key: str) -> str:
         """Change 'key' to secrets-compatible key field."""
         if not JujuVersion.from_environ().has_secrets:
@@ -559,11 +558,7 @@ class PgBouncerK8sCharm(CharmBase):
 
         peers = self.model.get_relation(PEER_RELATION_NAME)
         secret_key = self._translate_field_to_secret_key(key)
-        if scope == APP_SCOPE:
-            value = self.peer_relation_app.fetch_my_relation_field(peers.id, secret_key)
-        else:
-            value = self.peer_relation_unit.fetch_my_relation_field(peers.id, secret_key)
-        return value
+        return self.peer_relation_data(scope).fetch_my_relation_field(peers.id, secret_key)
 
     def set_secret(self, scope: Scopes, key: str, value: Optional[str]) -> Optional[str]:
         """Set secret from the secret storage."""
@@ -575,10 +570,7 @@ class PgBouncerK8sCharm(CharmBase):
 
         peers = self.model.get_relation(PEER_RELATION_NAME)
         secret_key = self._translate_field_to_secret_key(key)
-        if scope == APP_SCOPE:
-            self.peer_relation_app.update_relation_data(peers.id, {secret_key: value})
-        else:
-            self.peer_relation_unit.update_relation_data(peers.id, {secret_key: value})
+        self.peer_relation_data(scope).update_relation_data(peers.id, {secret_key: value})
 
     def remove_secret(self, scope: Scopes, key: str) -> None:
         """Removing a secret."""
@@ -587,10 +579,7 @@ class PgBouncerK8sCharm(CharmBase):
 
         peers = self.model.get_relation(PEER_RELATION_NAME)
         secret_key = self._translate_field_to_secret_key(key)
-        if scope == APP_SCOPE:
-            self.peer_relation_app.delete_relation_data(peers.id, [secret_key])
-        else:
-            self.peer_relation_unit.delete_relation_data(peers.id, [secret_key])
+        self.peer_relation_data(scope).delete_relation_data(peers.id, [secret_key])
 
     def push_tls_files_to_workload(self, update_config: bool = True) -> bool:
         """Uploads TLS files to the workload container."""
