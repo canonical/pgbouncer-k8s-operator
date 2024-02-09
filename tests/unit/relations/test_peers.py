a2d75484710a841c8734f592c84b8998a99ac59f
Dragomir Penev <dragomir.penev@canonical.com>
2024-02-09 03:43:41 +0200

    Add tests WIP

--- tests/unit/relations/test_peers.py
+++ tests/unit/relations/test_peers.py
@@ -2,12 +2,13 @@
 # See LICENSE file for licensing details.
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, PropertyMock, patch
 
 from ops.testing import Harness
 
 from charm import PgBouncerK8sCharm
-from constants import PEER_RELATION_NAME
+from constants import BACKEND_RELATION_NAME, PEER_RELATION_NAME
+from relations.peers import LEADER_ADDRESS_KEY
 
 
 class TestPeers(unittest.TestCase):
@@ -27,3 +28,40 @@ class TestPeers(unittest.TestCase):
 
     def tearDown(self):
         self.togggle_monitoring_patch.stop()
+
+    @patch(
+        "charm.PgBouncerK8sCharm.is_container_ready", new_callable=PropertyMock, return_value=True
+    )
+    @patch("charm.PgBouncerK8sCharm.render_pgb_config")
+    @patch("charm.PgBouncerK8sCharm.toggle_monitoring_layer")
+    def test_on_peers_changed(
+        self, toggle_monitoring_layer, render_pgb_config, is_container_ready
+    ):
+        self.harness.add_relation(BACKEND_RELATION_NAME, "postgres")
+        self.charm.peers._on_changed(Mock())
+        render_pgb_config.assert_called_once_with(reload_pgbouncer=True)
+        toggle_monitoring_layer.assert_called_once_with(False)
+        render_pgb_config.reset_mock()
+        toggle_monitoring_layer.reset_mock()
+
+        # Event will be deferred if the container is not ready
+        is_container_ready.return_value = False
+        event = Mock()
+        self.charm.peers._on_changed(event)
+        event.defer.assert_called_once_with()
+        assert not render_pgb_config.called
+        assert not toggle_monitoring_layer.called
+
+    @patch(
+        "charm.PgBouncerK8sCharm.unit_pod_hostname",
+        new_callable=PropertyMock,
+        return_value="test_pod_name",
+    )
+    @patch("charm.PgBouncerK8sCharm.generate_relation_databases")
+    def test_update_leader(self, _generate_relation_databases, unit_pod_hostname):
+        self.harness.add_relation(BACKEND_RELATION_NAME, "postgres")
+        # Will run the hook
+        self.harness.set_leader(True)
+
+        _generate_relation_databases.assert_called_once_with()
+        assert self.charm.peers.app_databag[LEADER_ADDRESS_KEY] == "test_pod_name"
--- tests/unit/test_pgb.py
+++ tests/unit/test_pgb.py
@@ -2,19 +2,34 @@
 # See LICENSE file for licensing details.
 
 import string
-import unittest
+from unittest import TestCase
+from unittest.mock import patch
 
 from charms.pgbouncer_k8s.v0 import pgb
 
-DATA_DIR = "tests/unit/data"
-TEST_VALID_INI = f"{DATA_DIR}/test.ini"
-PGB = "pgbouncer"
 
+class TestPgb(TestCase):
+    def test_parse_kv_string_to_dict(self):
+        assert {"test": "val", "another": "other"} == pgb.parse_kv_string_to_dict(
+            "test=val another=other"
+        )
 
-class TestPgb(unittest.TestCase):
+    def test_parse_dict_to_kv_string(self):
+        assert "test=val another=other" == pgb.parse_dict_to_kv_string(
+            {"test": "val", "another": "other"}
+        )
+
     def test_generate_password(self):
         pw = pgb.generate_password()
         self.assertEqual(len(pw), 24)
         valid_chars = string.ascii_letters + string.digits
         for char in pw:
             assert char in valid_chars
+
+    @patch("charms.pgbouncer_k8s.v0.pgb.md5")
+    def test_get_hashed_password(self, _md5):
+        hexdigest = _md5.return_value.hexdigest
+        hexdigest.return_value = "hashval"
+        assert "md5hashval" == pgb.get_hashed_password("user", "pass")
+        _md5.assert_called_once_with(b"passuser")
+        hexdigest.assert_called_once_with()
