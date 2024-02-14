4426e8f2ffed4275ce589b0e77a0321bd15a8311
Dragomir Penev <dragomir.penev@canonical.com>
2024-02-14 02:08:25 +0200

    Don't raise on blocked

--- tests/integration/relations/pgbouncer_provider/test_pgbouncer_provider.py
+++ tests/integration/relations/pgbouncer_provider/test_pgbouncer_provider.py
@@ -86,7 +86,7 @@ async def test_database_relation_with_charm_libraries(ops_test: OpsTest, pgb_cha
     await ops_test.model.add_relation(f"{PGB}:{BACKEND_RELATION_NAME}", f"{PG}:database")
 
     async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(apps=[PGB, PG], status="active")
+        await ops_test.model.wait_for_idle(apps=[PGB, PG], status="active", raise_on_blocked=False)
 
     # Relate the charms and wait for them exchanging some connection data.
     global client_relation
