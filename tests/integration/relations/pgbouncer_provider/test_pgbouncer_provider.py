ea1a86f209c0bfade2d98f03b7c355e727e0c73d
Dragomir Penev <dragomir.penev@canonical.com>
2025-07-30 00:44:50 +0300

    Blocking test app

--- tests/integration/relations/pgbouncer_provider/test_pgbouncer_provider.py
+++ tests/integration/relations/pgbouncer_provider/test_pgbouncer_provider.py
@@ -98,7 +98,7 @@ async def test_database_relation_with_charm_libraries(ops_test: OpsTest, charm):
     await ops_test.model.add_relation(f"{CLIENT_APP_NAME}:{FIRST_DATABASE_RELATION_NAME}", PGB)
 
     async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", raise_on_blocked=True)
+        await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active")
 
     # Check that on juju 3 we have secrets and no username and password in the rel databag
     if hasattr(ops_test.model, "list_secrets"):
